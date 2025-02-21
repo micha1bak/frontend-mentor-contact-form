1c08f52a360b3022d708853c50a6de081e103008
Michał Bąk <michalbak000@gmail.com>
2025-02-21 21:11:39 +0100

    input from html to js

---
+++ app.js
@@ -0,0 +1,11 @@
+const firstName = document.getElementById('first-name').value;
+const lastName = document.getElementById('last-name').value;
+const email = document.getElementById('email').value;
+
+const isQueryTypeSelected = document.querySelector('input[name="query-type"]:checked');
+
+const message = document.getElementById('message').value;
+
+const consent = document.getElementById('consent').checked;
+
+const submit = document.getElementById('submit');
