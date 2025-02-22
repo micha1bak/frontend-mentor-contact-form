a60f8433feaf40d064eca87787dfbd2f609efdf8
Michał Bąk <michalbak000@gmail.com>
2025-02-22 16:03:15 +0100

    added submit() method

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const firstName = document.getElementById('first-name').value;
-const lastName = document.getElementById('last-name').value;
-const email = document.getElementById('email').value;
 
-const isQueryTypeSelected = document.querySelector('input[name="query-type"]:checked');
+function submit() {
 
-const message = document.getElementById('message').value;
+  // type: string
+  const firstName = document.getElementById('first-name').value;
+  const lastName = document.getElementById('last-name').value;
+  const email = document.getElementById('email').value;
 
-const consent = document.getElementById('consent').checked;
+  // type boolean || true if any option selected, false otherwise
+  const isQueryTypeSelected = 
+  document.querySelector('input[name="query-type"]:checked') ? true : false;
 
-const submit = document.getElementById('submit');
+  // type: string
+  const message = document.getElementById('message').value;
+
+  // type: boolean || true if checked, false if not checked
+  const isCnsent = document.getElementById('consent').checked;
+
+  let text = '';
+  text += 'firstName: ' + firstName + '<br>';
+  text += 'lastName: ' + lastName + '<br>';
+  text += 'email: ' + email + '<br>';
+  text += 'isQueryTypeSelected: ' + isQueryTypeSelected + '<br>';
+  text += 'message: ' + message + '<br>';
+  text += 'isConsent: ' + isCnsent + '<br>';
+  document.getElementById('test').innerHTML = text;
+}
